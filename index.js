513e597d811a179c797a211463dd0a4b600710a7
Sameen Dhanimisetti <sameendhanimisetti@gmail.com>
2026-05-12 15:55:17 +0530

    Revert "add 18 above or not"
    This reverts commit 93c9276ceba0642b83169356886863c2b5c8dc27.

--- index.js
+++ index.js
@@ -4,13 +4,7 @@ const dob = '01/01/1990'
 function sayName() { 
     console.log(`My name is ${name}`)
 }
-function isabove18() {  
-    if (a > 18) {
-        console.log('You are above 18')
-    } else {
-        console.log('You are below 18')
-    }
-}
+
 function calculateAge() {
     const currentYear = new Date().getFullYear()
     const birthYear = parseInt(dob.split('/')[2])
