93c9276ceba0642b83169356886863c2b5c8dc27
Sameen Dhanimisetti <sameendhanimisetti@gmail.com>
2026-05-12 15:45:38 +0530

    add 18 above or not

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const a = 29
 const name = 'John Doe'
-
-const city = "Pune"
+const dob = '01/01/1990'
+function sayName() { 
+    console.log(`My name is ${name}`)
+}
+function isabove18() {  
+    if (a > 18) {
+        console.log('You are above 18')
+    } else {
+        console.log('You are below 18')
+    }
+}
